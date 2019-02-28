f6013e539d3f167397eeedfab9280d2eefb8d0f7
honghuiw <hhwang927@gmail.com>
2019-02-27 16:15:05 -0800

    update

--- pdf2txt_demo.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-with open('pdf2txt_demo.pdf', 'rb') as pdfFileObj:
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    for i in range(pdfReader.numPages):
-        pageObj = pdfReader.getPage(i)
-        print(pageObj.extractText())
-
-
